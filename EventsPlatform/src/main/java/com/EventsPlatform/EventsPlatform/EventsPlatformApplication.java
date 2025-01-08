21b2ab48349871a99b0d0a038c6eb9f10af35719
ludic <notetakingforeggs@gmail.com>
2025-01-08 12:15:08 +0000

    Feat: Init Spring Project

---
+++ EventsPlatform/src/main/java/com/EventsPlatform/EventsPlatform/EventsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.EventsPlatform.EventsPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsPlatformApplication.class, args);
+	}
+
+}
---
+++ EventsPlatform/src/test/java/com/EventsPlatform/EventsPlatform/EventsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EventsPlatform.EventsPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
