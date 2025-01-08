64274585ef0397907adc9df5251df8edb5d3426f
ludic <notetakingforeggs@gmail.com>
2025-01-08 12:30:48 +0000

    Feat: replace spring project

---
+++ backend/EventsPlatform/src/main/java/com/notetakingforeggs/EventsPlatform/EventsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.notetakingforeggs.EventsPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsPlatformApplication.class, args);
+	}
+
+}
