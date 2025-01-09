1fba12de30c030dce94bce1557d53837ce7631dd
ludic <notetakingforeggs@gmail.com>
2025-01-09 10:11:22 +0000

    Feat: entity classes defined with many to many relationship

--- backend/EventsPlatform/src/main/java/com/notetakingforeggs/EventsPlatform/model/AppUser.java
+++ backend/EventsPlatform/src/main/java/com/notetakingforeggs/EventsPlatform/model/AppUser.java
@@ -1,9 +1,20 @@
 package com.notetakingforeggs.EventsPlatform.model;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.util.List;
 @Entity
 @Data
 public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "attendees")
+    private List<Event> events;
+
 }
--- backend/EventsPlatform/src/main/java/com/notetakingforeggs/EventsPlatform/model/Event.java
+++ backend/EventsPlatform/src/main/java/com/notetakingforeggs/EventsPlatform/model/Event.java
@@ -1,4 +1,31 @@
 package com.notetakingforeggs.EventsPlatform.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Entity
+@Data
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String eventName;
+
+    private ZonedDateTime startDate;
+
+    private ZonedDateTime endDate;
+
+    @ManyToMany
+    @JoinTable(
+        name = "event_attendees",
+        joinColumns = @JoinColumn(name = "event_id"),
+        inverseJoinColumns =@JoinColumn(name = "user_id")
+    )
+    private List<AppUser> attendees;
+
 }
