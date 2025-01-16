9ebf98110a057f5bee3260905c65a08e2d2baf4b
ludic <notetakingforeggs@gmail.com>
2025-01-16 13:28:55 +0000

    Fix: changing ndk version, min sdk version in app level build.gradle, and gradle sitribution version in gradle-wrapper.properties

--- frontend/lib/pages/login_page.dart
+++ frontend/lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:events_platform_frontend/services/auth/auth_service.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -13,11 +14,23 @@ class LoginPage extends StatelessWidget {
           children: [
             // App Name
             Text("Events Platform", style: TextStyle(fontSize: 36)),
-            SizedBox(height:50),
+            SizedBox(height: 50),
             // Logo
-            Icon(Icons.calendar_view_week_sharp, size: 90,),
-
+            Icon(
+              Icons.calendar_view_week_sharp,
+              size: 90,
+            ),
+            SizedBox(
+              height: 100,
+            ),
             // Google Sign In button
+            ElevatedButton(
+              onPressed: () => AuthService().signInWithGoogle(),
+              child: Text(
+                "Google Sign In",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
             //
           ],
         ),
