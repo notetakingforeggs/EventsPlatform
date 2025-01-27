9b72046ed61164a0c81712c93a7b1c95a1b8e11c
ludic <notetakingforeggs@gmail.com>
2025-01-27 13:50:36 +0000

    WIP: implementing applinks

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,20 +3,49 @@ import 'package:events_platform_frontend/theme/light_mode.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:app_links/app_links.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+
   // The line below came with the firebase snippet, but may not be needed for now?
   // show defaultTargetPlatform, kIsWeb, TargetPlatform;
   runApp(const MyApp());
 }
 
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+
+
+
+class _MyAppState extends State<MyApp> {
+  late AppLinks _appLinks;
+  String? _deepLink;
+
+  @override
+  void initState() {
+    super.initState();
+    initDeepLinks();
+  }
+  void initDeepLinks(){
+    _appLinks.uriLinkStream.listen((Uri? uri){
+      if(uri != null){
+        setState(() {
+          _deepLink = uri.toString();
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
--- frontend/lib/services/custom_tabs/CustomTabs1.dart
+++ frontend/lib/services/custom_tabs/CustomTabs1.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_custom_tabs/flutter_custom_tabs_lite.dart';
 
 
-void launchURLL(BuildContext context) async {
+void launchGoogleAuthCustomTab(BuildContext context, String uri) async {
   final theme = Theme.of(context);
   try {
     await launchUrl(
-      Uri.parse('https://accounts.google.com/o/oauth2/v2/auth?client_id=8374014814-f2ql0dltfnijdfdc74487qhq6u2ooocd.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fapi%2Fv1%2Foauth2%2Fcallback&response_type=code&scope=openid+profile+email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar&access_type=offline'),
-      // Uri.parse('https://www.google.com'),
+      Uri.parse(uri),
       options: LaunchOptions(
         barColor: theme.colorScheme.surface,
         onBarColor: theme.colorScheme.onSurface,
