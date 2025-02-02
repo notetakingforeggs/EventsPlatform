106843c16960e6a3da9cc83d56a65d62090349c5
ludic <notetakingforeggs@gmail.com>
2025-02-02 09:07:58 +0000

    Feat: initialising add_event page

---
+++ frontend/lib/pages/add_event/add_event_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import './add_event_controller.dart';
+
+class AddEventPage extends StatefulWidget {
+    static const name = "AddEventPage";
+
+    static final GoRoute route = GoRoute(
+      path: name,
+      name: name,
+      builder: (context, state) {
+        return const AddEventPage();
+      },
+    );
+
+    static Future navigate(BuildContext context) {
+        return context.pushNamed(name);
+    }
+
+    const AddEventPage({super.key});
+
+    @override
+    State<AddEventPage> createState() => _AddEventPageState();
+}
+
+class _AddEventPageState extends State<AddEventPage> {
+
+    final AddEventController _controller = AddEventController();
+
+    @override
+    void initState() {
+      super.initState();
+    }
+
+    @override
+     Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(),
+          body: Column(),
+        );
+     }
+}
