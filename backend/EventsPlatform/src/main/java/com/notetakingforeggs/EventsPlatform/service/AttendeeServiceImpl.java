5299869575ab3be9029c0440d2077d3e44ddcca3
ludic <notetakingforeggs@gmail.com>
2025-02-04 13:58:15 +0000

    Refactor: going back to objects in the join table.

--- backend/EventsPlatform/src/main/java/com/notetakingforeggs/EventsPlatform/service/AttendeeServiceImpl.java
+++ backend/EventsPlatform/src/main/java/com/notetakingforeggs/EventsPlatform/service/AttendeeServiceImpl.java
@@ -6,9 +6,13 @@ import com.notetakingforeggs.EventsPlatform.repository.AttendeeRepository;
 public class AttendeeServiceImpl implements AttendeeService {
 
     private final AttendeeRepository repository;
+    private final UserService userService;
+    private final EventService eventService;
 
-    public AttendeeServiceImpl(AttendeeRepository repository) {
+    public AttendeeServiceImpl(AttendeeRepository repository, UserService userService, EventService eventService) {
         this.repository = repository;
+        this.userService = userService;
+        this.eventService = eventService;
     }
 
     @Override
@@ -16,8 +20,8 @@ public class AttendeeServiceImpl implements AttendeeService {
         Attendee newAttendee = new Attendee();
 
         // TODO this fucked up becasue i havent decided if i am using objects or ids in the join table. do this after lunch
-        newAttendee.setEventId(eventId);
-        newAttendee.setUserGoogleId(userGoogleId);
+        newAttendee.setUser(userService.getByGoogleUid(userGoogleId));
+        newAttendee.setEvent(eventService.getById(eventId));
         return repository.save(newAttendee);
 
     }
